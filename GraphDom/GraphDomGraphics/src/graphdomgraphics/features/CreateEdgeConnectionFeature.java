7285fe935c16e5a35b44f5d87d071a019a3b1632
DAVID GONZALO BLANCO <dgonzalo@servexternos.isban.es>
2015-12-09 12:31:45 +0100

    Can add && create ecore

--- GraphDom/GraphDomGraphics/src/graphdomgraphics/features/CreateEdgeConnectionFeature.java
+++ GraphDom/GraphDomGraphics/src/graphdomgraphics/features/CreateEdgeConnectionFeature.java
@@ -33,7 +33,7 @@ public class CreateEdgeConnectionFeature extends AbstractCreateConnectionFeature
 		Node source = getNode(context.getSourceAnchor());
 		Node target = getNode(context.getTargetAnchor());
 		
-        if (source != null && target != null && source != target) {
+        if ((source != null && target != null && source != target) && !source.getConnectedEdges().contains(target)) {
 		  	return true;
 		 }
 		
