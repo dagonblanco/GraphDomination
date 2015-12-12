19d81106cb13ede4ac0ae427b2b9d629a650e60d
David <David@192.168.1.3>
2015-12-12 23:19:45 +0100

    Actualización del trabajo en casa

--- GraphDom/GraphDomGraphics/src/graphdomgraphics/common/ExampleUtil.java
+++ GraphDom/GraphDomGraphics/src/graphdomgraphics/common/ExampleUtil.java
@@ -15,6 +15,17 @@
  *******************************************************************************/
 package graphdomgraphics.common;
 
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.graphiti.mm.algorithms.styles.Color;
 import org.eclipse.graphiti.mm.pictograms.Diagram;
 import org.eclipse.graphiti.services.Graphiti;
@@ -86,5 +97,28 @@ public class ExampleUtil {
 	 */
 	private static Shell getShell() {
 		return PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+	}
+	
+	public static EObject getDiagramDO(final Diagram d) {
+		//TODO: ¿Y esto?
+		return null;
+	}
+
+	public static void saveToModelFile(final EObject obj, final Diagram d) throws CoreException, IOException {
+		URI uri = d.eResource().getURI();
+		uri = uri.trimFragment();
+		uri = uri.trimFileExtension();
+		uri = uri.appendFileExtension("model");
+		ResourceSet rSet = d.eResource().getResourceSet();
+		final IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
+		IResource file = workspaceRoot.findMember(uri.toPlatformString(true));
+		if (file == null || !file.exists()) {
+			Resource createResource = rSet.createResource(uri);
+			createResource.save(new HashMap());
+			createResource.setTrackingModification(true);
+		}
+		final Resource resource = rSet.getResource(uri, true);
+		resource.getContents().add(obj);
+
 	}
 }
