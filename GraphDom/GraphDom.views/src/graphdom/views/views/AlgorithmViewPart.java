e85572320c18c5be0dbd91e4fdff1aa6bcc925fd
David <David@192.168.1.3>
2016-01-03 00:07:02 +0100

    Config @ home

---
+++ GraphDom/GraphDom.views/src/graphdom/views/views/AlgorithmViewPart.java
@@ -0,0 +1,24 @@
+package graphdom.views.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class AlgorithmViewPart extends ViewPart {
+
+	public AlgorithmViewPart() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
